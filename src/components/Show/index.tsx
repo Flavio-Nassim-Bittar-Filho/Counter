b72378c69eb68f8b5551705f538d5785011feefe
Flávio Nassim Bittar Filho <flavionassimbittarfilho@gmail.com>
2023-03-07 12:03:41 -0300

    deploy project

--- src/components/Show/index.tsx
+++
@@ -1,6 +0,0 @@
-import { useGlobal } from '../../context/hooks/useGlobal'
-
-export const Show = () => {
-  const { counter } = useGlobal()
-  return <h1 className='text-8xl font-light'>{counter}</h1>
-}
