f7e89028b7571c6b6b5714c781d484929eaad165
Flávio Nassim Bittar Filho <flavionassimbittarfilho@gmail.com>
2023-03-09 08:52:34 -0300

    Update - add "base" for building

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,17 +4,9 @@ interface Btn {
   onClick?: () => any
 }
 
-export const Button = ({
-  children,
-  className = 'bg-slate-400 ',
-  onClick,
-}: Btn) => {
+export const Button = ({ children, className = 'bg-slate-400 ', onClick }: Btn) => {
   return (
-    <button
-      className={`w-16 h-16 rounded-full
-      ${className ? className : ''}`}
-      onClick={onClick}
-    >
+    <button className={`w-16 h-16 rounded-full ${className ? className : ''}`} onClick={onClick}>
       {children}
     </button>
   )
